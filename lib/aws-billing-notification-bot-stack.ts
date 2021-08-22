f86884a2bb586e3cae214fb774d2604bda2b76b3
tk1024 <tk1024@users.noreply.github.com>
2021-08-23 04:59:53 +0900

    update policy

--- lib/aws-billing-notification-bot-stack.ts
+++ lib/aws-billing-notification-bot-stack.ts
@@ -4,12 +4,24 @@ import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs'
 import * as cdk from '@aws-cdk/core'
 import { Duration } from '@aws-cdk/core'
 import { LambdaFunction } from '@aws-cdk/aws-events-targets'
+import * as iam from '@aws-cdk/aws-iam'
+import { Effect, PolicyStatement } from '@aws-cdk/aws-iam'
 
 const WEBHOOK_URL = process.env.WEBHOOK_URL!
 
 export class AwsBillingNotificationBotStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const role = new iam.Role(this, 'NotificationBotRole', {
+      assumedBy: new iam.ServicePrincipal("lambda"),
+    });
+
+    role.addToPolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: ["ce:GetCostAndUsage"],
+      resources: ["*"]
+    }));
 
     const lambdaFn = new NodejsFunction(this, 'NotificationBot', {
       entry: 'src/lambda/app.ts',
@@ -25,12 +37,13 @@ export class AwsBillingNotificationBotStack extends cdk.Stack {
       environment: {
         WEBHOOK_URL,
       },
+      role,
     });
 
     const rule = new Rule(this, 'Schedule Rule', {
       schedule: Schedule.cron({ minute: '0', hour: '0' }),
-     });
+    });
 
-     rule.addTarget(new LambdaFunction(lambdaFn));
+    rule.addTarget(new LambdaFunction(lambdaFn));
   }
 }
